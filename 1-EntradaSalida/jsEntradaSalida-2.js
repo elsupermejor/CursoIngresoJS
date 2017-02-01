f9c1f9070af1d5e4379b1c6914f10bcdb5524e51
elsupermejor <federicoeescobar@hotmail.com>
2017-02-01 12:02:26 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,12 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
-{
+{	var nombre;//Defino variable
+	nombre="Federico";//Cargo variable, pero no es necesario al utilizar el prompt
+	nombre=prompt("ingresa tu nombre", "natalia natalia");
+	alert("nombre  "+nombre);
+	//esto es un comentario de una niña
+	var person = prompt("ingresa tu nombre", "natalia natalia");
 	
 }
 
