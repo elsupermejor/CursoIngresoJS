d242846936f67c503169e769183f36b38057d13b
elsupermejor <federicoeescobar@hotmail.com>
2017-02-08 08:59:20 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,22 +3,54 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{	var numerouno;
+	var numerodos;
+	var resultado;
+	numerouno= document.getElementById("numeroUno").value;
+	numerodos= document.getElementById("numeroDos").value;
+	numerouno= parseInt(numerouno);
+	numerodos= parseInt(numerodos);
+	resultado=(numerouno+numerodos);
+	alert("El resultado es "+resultado);
 	
 }
 
 function restar()
-{
+{var numerouno;
+	var numerodos;
+	var resultado;
+	numerouno= document.getElementById("numeroUno").value;
+	numerodos= document.getElementById("numeroDos").value;
+	numerouno= parseInt(numerouno);
+	numerodos= parseInt(numerodos);
+	resultado=(numerouno-numerodos);
+	alert("El resultado es "+resultado);
 	
 }
 
 function multiplicar()
 { 
-	
+	var numerouno;
+	var numerodos;
+	var resultado;
+	numerouno= document.getElementById("numeroUno").value;
+	numerodos= document.getElementById("numeroDos").value;
+	numerouno= parseInt(numerouno);
+	numerodos= parseInt(numerodos);
+	resultado=(numerouno*numerodos);
+	alert("El resultado es "+resultado);
 }
 
 function dividir()
 {
-	
+	var numerouno;
+	var numerodos;
+	var resultado;
+	numerouno= document.getElementById("numeroUno").value;
+	numerodos= document.getElementById("numeroDos").value;
+	numerouno= parseInt(numerouno);
+	numerodos= parseInt(numerodos);
+	resultado=(numerouno/numerodos);
+	alert("El resultado es "+resultado);
 }
 
