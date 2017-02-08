9108128d154cd257d5f415b676f09599dc2c5b75
elsupermejor <federicoeescobar@hotmail.com>
2017-02-08 12:19:49 -0300

    Add files via upload

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -4,11 +4,33 @@ B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
-{
+{var numerouno;
+	var numerodos;
+	var numerotres;
+	var resultado;
+	numerouno= document.getElementById("PrecioUno").value;
+	numerodos= document.getElementById("PrecioDos").value;
+	numerotres= document.getElementById("PrecioTres").value;
+	numerouno= parseInt(numerouno);
+	numerodos= parseInt(numerodos);
+	numerotres= parseInt(numerotres);
+	resultado=(numerouno+numerodos+numerotres);
+	alert("la suma es "+resultado);
 	
 }
 function Promedio () 
-{
+{var numerouno;
+	var numerodos;
+	var	numerotres;
+	var resultado;
+	var promedio;
+	numerouno=document.getElementById('PrecioUno');
+	numerodos=document.getElementById('PrecioDos');
+	numerotres=document.getElementById('PrecioTres');
+	numerouno=parseInt(numerouno);
+	numerodos=parseInt(numerodos);
+	numerotres=parseInt()
+
 	
 }
 function PrecioFinal () 
