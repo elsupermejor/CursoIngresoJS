04c9a32460b7cb87fcf995aebe21360751416a3c
elsupermejor <federicoeescobar@hotmail.com>
2017-02-14 15:28:54 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,5 +1,12 @@
 function Mostrar()
-{
+{	var edad;
+	var estado;
+	edad=document.getElementById('edad').value;
+	estado=document.getElementById('estadoCivil').value;
+	if(edad>17 && estado=="Soltero") 
+	{
+		alert("Es soltero y no es menor");
+	}
 //tomo la edad  
 
 	
