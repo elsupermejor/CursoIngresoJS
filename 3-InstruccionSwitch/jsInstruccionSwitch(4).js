3439c63d2656a4a9110f1feedaae84a06216722b
elsupermejor <federicoeescobar@hotmail.com>
2017-02-14 15:30:31 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -2,6 +2,24 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño)
+{
+	case "Enero":
+	case "Marzo":
+	case "Mayo":
+	case "Julio":
+	case "Agosto":
+	case "Octubre":
+	case "Diciembre":
+	alert("tiene 31 días.");
+	break;
+	case "Febrero":
+	alert("tiene 28 días.");
+	break;
+	default:
+	alert("tiene 30 días");
+	break;
+}
 
 //alert (mesDelAño);
 	
