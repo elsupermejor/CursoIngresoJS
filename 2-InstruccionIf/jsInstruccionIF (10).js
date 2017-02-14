ae7b6a0e1e2aa34ac528b01c85787560ff5c613c
elsupermejor <federicoeescobar@hotmail.com>
2017-02-14 12:06:17 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,33 @@
 function Mostrar()
-{
+{var random;
+	random=Math.floor(Math.random() * 10) + 1;
+/*	if(random>8)
+	{
+		alert(random+" exelente");
+	}
+	else
+	{
+		if(random<4)
+			{alert(random+" vamos, la proxima se puede");
+			}
+		else
+			{alert(random+" Aprovo");
+			}
+	}*/
 	//Genero el número RANDOM entre 1 y 10 
 	
-
+if(random>8)
+{
+	alert("nota: "+random+ " exelente");
+}
+	else
+	{if(random<4)
+		{
+		alert("nota: "+random+ " Vamos, la proxima se puede");
+		}
+		else
+		{
+		alert("nota: "+random+ " Aprovo");
+		}
+	}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,7 @@
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero
+	numero=Math.floor(Math.random() * 10) + 1;//Genero el número RANDOM entre 1 y 10 
+	alert("su numero random es "+numero)	
 
 }//FIN DE LA FUNCIÓN
