dc7f7720282999f438d652804f790c24ba7dc414
elsupermejor <federicoeescobar@hotmail.com>
2017-02-21 09:31:50 -0300

    Add files via upload

---
+++ soluciones.js
@@ -0,0 +1,34 @@
+{
+	var contador=0;
+	var numero;
+	var max;
+	var min;
+	while()
+		{
+		if(contador==0)
+		{
+		max=numero;
+		min=numero:
+		}
+		else
+		{
+			if(numero>max)
+			max=numero;
+			if(numero<min)
+			min=numero;
+		}
+
+
+
+
+
+
+
+
+		}
+
+
+
+
+
+}
