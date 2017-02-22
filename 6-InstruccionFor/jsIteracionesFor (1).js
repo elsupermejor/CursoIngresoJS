95974cfb8eef759964dda066cce31e93b82f7031
elsupermejor <federicoeescobar@hotmail.com>
2017-02-22 12:05:08 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,31 @@
 function Mostrar()
 {
 
+//primer ejemplo
+/*for(var cont = 0 ; cont < 5; cont ++)
+{
+	console.log(cont);
+}*/ 
+
+//segundo ejemplo
+/*var cont=0
+for( ;cont<5 ; cont++)
+{
+	console.log(cont);
+}*/ 
+
+//tercer ejemplo que no funciona.
+/*var cont=0
+for( ; ; )
+	if(cont==5)
+	{
+		break;
+	}
+	cont++;
+	console.log(cont);
+	}*/
+var cont
+for(cont=1;cont<11;cont++)
+	alert("los numeros son " + cont );
+
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,7 @@
 function Mostrar()
 {
-
+	var cont;
+	for(contador=10;contador>0;contador--)
+		alert("tus numeros son " +contador);
 
 }
